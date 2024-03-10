11e414ba45216b516b88dc8b1d153dc03c350fd7
artkoz <artko@mgslab.com>
2024-03-10 13:00:14 +0200

    Add files via upload
    changes for kde 6.0/dolphin 24.02 support. tested on ArchLinux

--- stlcreator.cpp
+++ stlcreator.cpp
@@ -7,43 +7,47 @@
 #include <iostream>
 #include <string>
 
+#include <KPluginFactory>
+
+K_PLUGIN_CLASS_WITH_JSON(StlCreator, "stlthumbnail.json")
+
 Q_LOGGING_CATEGORY(LOG_STL, "com.stl-thumb-kde")
 
-extern "C"
+StlCreator::StlCreator(QObject *parent, const QVariantList &args)
+    : KIO::ThumbnailCreator(parent, args)
 {
-    Q_DECL_EXPORT ThumbCreator *new_creator()
-    {
-        return new StlCreator();
-    }
-};
+}
 
-StlCreator::StlCreator() = default;
-
-StlCreator::~StlCreator() = default;
-
-bool StlCreator::create(const QString &path, int width, int height, QImage &img)
+KIO::ThumbnailResult StlCreator::create(const KIO::ThumbnailRequest &request)
 {
+    const QString path = request.url().toLocalFile();
     std::string path_string = path.toStdString();
+    qCWarning(LOG_STL) << "STL-THUMB :: enter library";
     const char* path_cstring = path_string.c_str();
+    int width  = request.targetSize().width() * request.devicePixelRatio();
+    int height = request.targetSize().height() * request.devicePixelRatio();
 
     int img_size = width * height * 4;
     img_buf = (uchar *) malloc(img_size);
-    if (img_buf == NULL) return false;
+    if (img_buf == NULL) return KIO::ThumbnailResult::fail();
 
     if (!render_to_buffer(img_buf, width, height, path_cstring)) {
         qCWarning(LOG_STL) << "STL-THUMB :: lib returned error\n";
-        return false;
+        return KIO::ThumbnailResult::fail();
     }
 
-    img = QImage(img_buf,
+    QImage img = QImage(img_buf,
                  width,
                  height,
                  QImage::Format_RGBA8888,
                  (QImageCleanupFunction) &clean,
                  (void*) this);
 
-    return true;
+//    return true;
+    qCWarning(LOG_STL) << "STL-THUMB :: normal exit\n";
+    return KIO::ThumbnailResult::pass(img);
 }
+
 
 // Cleanup function provided to QImage to free the buffer after it is done
 void clean(void *info)
@@ -51,3 +55,5 @@ void clean(void *info)
     StlCreator * self = static_cast<StlCreator*>(info);
     free(self->img_buf);
 }
+
+#include "stlcreator.moc"
--- stlcreator.h
+++ stlcreator.h
@@ -1,20 +1,18 @@
 #ifndef _STLCREATOR_H_
 #define _STLCREATOR_H_
 
-#include <KIO/ThumbCreator>
+#include <KIO/ThumbnailCreator>
 
 #include <QLoggingCategory>
 Q_DECLARE_LOGGING_CATEGORY(LOG_STL)
 
-class StlCreator : public ThumbCreator
+class StlCreator : public KIO::ThumbnailCreator
 {
-    public:
-        StlCreator();
-        ~StlCreator() override;
-
-        bool create(const QString &path, int width, int height, QImage &img) override;
-
-        uchar *img_buf = NULL;
+ Q_OBJECT
+public:
+    StlCreator(QObject *parent, const QVariantList &args);
+    KIO::ThumbnailResult create(const KIO::ThumbnailRequest &request) override;
+    uchar *img_buf = NULL;
 };
 
 void clean(void *info);
